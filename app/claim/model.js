8ee68f8861b61125639f601b3ce6f2efcc1c0d16
Marcellin Nshimiyimana <mars@fusemachines.com>
2016-06-07 02:12:50 -0400

    remove all files

--- app/claim/model.js
+++
@@ -1,8 +0,0 @@
-import Model from 'ember-data/model';
-
-const {
-	attr
-} = Ember;
-export default Model.extend({
-  content: attr('string')
-});
--- app/index/controller.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-	validClaim: 'This Is How We Do It',
-	validDefense: 'Coming soon'
-});
